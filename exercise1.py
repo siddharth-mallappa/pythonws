307a43474b94e13d0fd6d448320afdfa0fa66775
siddharth-mallappa <39707625+siddharth-mallappa@users.noreply.github.com>
2019-06-20 13:01:01 +0530

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -5,14 +5,10 @@ def myinput():
      user=input("Enter the user name")
      return dbname,username,passwd,user
 
-def connect( dbname,username,passwd,user ):
 
-    
-    print("Databasename={}  username={} password={} user={}".format( dbname, username, passwd,user))
 
 dbname, username, passwd,user=myinput()
-connect( dbname, username, passwd, user )
-    
+print("Databasename={}  username={} password={} user={}".format( dbname, username, passwd,user))    
     
     
     
